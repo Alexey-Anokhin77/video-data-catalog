0b650df196529581d4cfaab65ec4505eb40fc684
Alexey Anokhin <alexey.anokhin88@gmail.com>
2025-04-28 20:53:37 +0300

    Update main and added router

--- v-data-catalog/main.py
+++ v-data-catalog/main.py
@@ -3,11 +3,13 @@ from fastapi import (
     Request,
 )
 
-from schemas.video_catalog import VideoCatalog
+from api import router as api_router
 
 app = FastAPI(
     title="Video Data Catalog",
 )
+
+app.include_router(api_router)
 
 
 @app.get("/")
@@ -23,41 +25,3 @@ def read_root(
         "message": f"Hello {name}",
         "docs": str(docs_url),
     }
-
-
-FILM_CATALOG = [
-    VideoCatalog(
-        id_film=1,
-        title_film="Интерстеллар",
-        description_film="Команда астронавтов отправляется сквозь червоточину в поисках нового дома для "
-        "пределов человеческой выносливости.",
-        time_film=2.50,
-    ),
-    VideoCatalog(
-        id_film=2,
-        title_film="Матрица",
-        description_film="Хакер узнаёт, что мир вокруг — иллюзия, созданная искусственным разумом, "
-        "и присоединяется к борьбе за освобождение человечества от машинного контроля.",
-        time_film=2.45,
-    ),
-    VideoCatalog(
-        id_film=3,
-        title_film="Начало",
-        description_film="Группа специалистов проникает в подсознание людей, используя технику погружения в "
-        "сновидения.",
-        time_film=2.28,
-    ),
-]
-
-
-@app.get(
-    "/video-catalog/{id_film}",
-)
-@app.get(
-    "/video-catalog/{id_film}/",
-)
-def read_film_id(id_film: int) -> VideoCatalog | None:
-    film: VideoCatalog | None = next(
-        (film for film in FILM_CATALOG if film.id_film == id_film), None
-    )
-    return film
