ed8231b93b52b92a61a3d60fd6e1216df3a354fe
Alexey Anokhin <alexey.anokhin88@gmail.com>
2025-04-30 23:02:56 +0300

    Update

--- v-data-catalog/api/api_v1/video_catalog/dependencies.py
+++ v-data-catalog/api/api_v1/video_catalog/dependencies.py
@@ -5,16 +5,16 @@ from api.api_v1.video_catalog.crud import FILM_CATALOG
 from schemas.video_catalog import VideoCatalog
 
 
-def read_film_id(
-    id_film: int,
+def read_film_slug(
+    slug: str,
 ) -> VideoCatalog | None:
     film: VideoCatalog | None = next(
-        (film for film in FILM_CATALOG if film.id_film == id_film),
+        (film for film in FILM_CATALOG if film.slug == slug),
         None,
     )
     if film:
         return film
     raise HTTPException(
         status_code=status.HTTP_404_NOT_FOUND,
-        detail=f"URL{id_film!r} not found",
+        detail=f"URL{slug!r} not found",
     )
