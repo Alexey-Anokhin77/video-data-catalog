4ca29990fc98aeb04afec2f3b61071f182f149cc
Alexey Anokhin <alexey.anokhin88@gmail.com>
2025-05-02 17:29:13 +0300

    Add new class and new  storage

--- v-data-catalog/api/api_v1/video_catalog/crud.py
+++ v-data-catalog/api/api_v1/video_catalog/crud.py
@@ -1,4 +1,9 @@
-from schemas.video_catalog import VideoCatalog
+from pydantic import BaseModel
+
+from schemas.video_catalog import (
+    VideoCatalog,
+    VideoCreate,
+)
 
 FILM_CATALOG = [
     VideoCatalog(
@@ -22,3 +27,53 @@ FILM_CATALOG = [
         time_film=2.28,
     ),
 ]
+
+
+class VideosStorage(BaseModel):
+    slug_to_video: dict[str, VideoCatalog] = {}
+
+    # Получение списка
+    def get(self) -> list[VideoCatalog]:
+        return list(self.slug_to_video.values())
+
+    # Получение по slug
+    def get_by_slug(self, slug: str) -> VideoCatalog | None:
+        return self.slug_to_video.get(slug)
+
+    # Создание нового видео
+    def create(self, video_in: VideoCreate) -> VideoCatalog:
+        film = VideoCatalog(
+            **video_in.model_dump(),
+        )
+        self.slug_to_video[film.slug] = film
+        return film
+
+
+storage = VideosStorage()
+
+storage.create(
+    VideoCreate(
+        slug="interstellar-2014",
+        title_film="Интерстеллар",
+        description_film="about",
+        time_film=2.50,
+    ),
+)
+storage.create(
+    VideoCreate(
+        slug="the-matrix-1999",
+        title_film="Матрица",
+        description_film="Хакер узнаёт, что мир вокруг — иллюзия, созданная искусственным разумом, "
+        "и присоединяется к борьбе за освобождение человечества от машинного контроля.",
+        time_film=2.45,
+    ),
+)
+storage.create(
+    VideoCreate(
+        slug="Inception 2010",
+        title_film="Начало",
+        description_film="Группа специалистов проникает в подсознание людей, используя технику погружения в "
+        "сновидения.",
+        time_film=2.28,
+    ),
+)
