87957e06c16ab0659b4c3834363934a556edf27d
Alexey Anokhin <alexey.anokhin88@gmail.com>
2025-05-10 20:34:04 +0300

    Create new class Update

--- v-data-catalog/schemas/video_catalog.py
+++ v-data-catalog/schemas/video_catalog.py
@@ -1,20 +1,24 @@
 from typing import Annotated
 
-from annotated_types import Len
+from annotated_types import (
+    Len,
+    MaxLen,
+)
 from pydantic import (
     BaseModel,
-    Field,
 )
 
 
 class FilmInfoBase(BaseModel):
-    slug: str
-    title_film: str
-    description_film: str
     time_film: float
+    title_film: str
+    description_film: Annotated[
+        str,
+        MaxLen(1000),
+    ] = ""
 
 
-class VideoCreate(FilmInfoBase):
+class MovieCreate(FilmInfoBase):
     """
     Модель создания фильма
     """
@@ -23,12 +27,24 @@ class VideoCreate(FilmInfoBase):
         str,
         Len(min_length=3, max_length=100),
     ]
-    title_film: Annotated[str, Field(description="Название фильма")]
-    description_film: Annotated[str, Field(description="Описание фильма")]
-    time_film: Annotated[float, Field(gt=0)]
 
 
-class VideoCatalog(FilmInfoBase):
+class MovieUpdate(FilmInfoBase):
+    """
+    Модель для обновления информации о фильме.
+    """
+
+    time_film: float
+    title_film: str
+    description_film: Annotated[
+        str,
+        MaxLen(1000),
+    ]
+
+
+class Movie(FilmInfoBase):
     """
     Модель фильма
     """
+
+    slug: str
