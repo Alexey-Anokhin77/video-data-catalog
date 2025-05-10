c0e5cd973c807f261740dca4526801b12b2ac94e
Alexey Anokhin <alexey.anokhin88@gmail.com>
2025-05-10 20:34:15 +0300

    Update

--- v-data-catalog/api/api_v1/video_catalog/dependencies.py
+++ v-data-catalog/api/api_v1/video_catalog/dependencies.py
@@ -2,16 +2,16 @@ from fastapi import HTTPException
 from starlette import status
 
 from api.api_v1.video_catalog.crud import storage
-from schemas.video_catalog import VideoCatalog
+from schemas.video_catalog import Movie
 
 
 def read_film_slug(
     slug: str,
-) -> VideoCatalog:
-    film: VideoCatalog | None = storage.get_by_slug(slug=slug)
+) -> Movie:
+    film: Movie | None = storage.get_by_slug(slug=slug)
     if film:
         return film
     raise HTTPException(
         status_code=status.HTTP_404_NOT_FOUND,
-        detail=f"URL{slug!r} not found",
+        detail=f"Film{slug!r} not found",
     )
