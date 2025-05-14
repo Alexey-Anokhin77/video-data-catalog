a157a488e3888c64c727d88b36d0f4ae10d2c6e5
Alexey Anokhin <alexey.anokhin88@gmail.com>
2025-05-14 14:26:04 +0300

    Create save_state and read json

--- v-data-catalog/api/api_v1/video_catalog/crud.py
+++ v-data-catalog/api/api_v1/video_catalog/crud.py
@@ -1,5 +1,6 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, ValidationError
 
+from core.config import MOVIES_STORAGE_FILEPATH
 from schemas.video_catalog import (
     Movie,
     MovieCreate,
@@ -8,8 +9,23 @@ from schemas.video_catalog import (
 )
 
 
-class VideosStorage(BaseModel):
+class VideoStorage(BaseModel):
     slug_to_video: dict[str, Movie] = {}
+
+    # Метод сохранения данных локально на жесткий диск
+    def save_state(self) -> None:
+        MOVIES_STORAGE_FILEPATH.write_text(
+            self.model_dump_json(indent=2), encoding="utf-8"
+        )
+
+    # Кастомный инициализатор на проверку существования и чтения файла
+    @classmethod
+    def from_state(cls) -> "VideoStorage":
+        if not MOVIES_STORAGE_FILEPATH.exists():
+            return VideoStorage()
+        return cls.model_validate_json(
+            MOVIES_STORAGE_FILEPATH.read_text(encoding="utf-8")
+        )
 
     # Получение списка
     def get(self) -> list[Movie]:
@@ -25,10 +41,12 @@ class VideosStorage(BaseModel):
             **video_in.model_dump(),
         )
         self.slug_to_video[film.slug] = film
+        self.save_state()
         return film
 
     def delete_by_slug(self, slug: str) -> None:
         self.slug_to_video.pop(slug, None)
+        self.save_state()
 
     def delete(self, film: Movie) -> None:
         self.delete_by_slug(slug=film.slug)
@@ -42,6 +60,7 @@ class VideosStorage(BaseModel):
         # Обновляем поля
         for field_name, value in update_data.items():
             setattr(film, field_name, value)
+        self.save_state()
         return film
 
     def partial_update(
@@ -54,42 +73,47 @@ class VideosStorage(BaseModel):
         # Обновляем только указанные поля
         for field_name, value in update_data.items():
             setattr(film, field_name, value)
+        self.save_state()
         return film
 
 
-storage = VideosStorage()
+try:
+    storage = VideoStorage.from_state()
+except ValidationError:
+    storage = VideoStorage()
+    storage.save_state()
 
-storage.create(
-    MovieCreate(
-        slug="interstellar-2014",
-        title_film="Интерстеллар",
-        genre="Научная фантастика",
-        production_year=2014,
-        description_film="Фильм «Интерстеллар» рассказывает историю группы астронавтов, отправившихся сквозь "
-        "червоточину в космос в поисках нового дома для человечества, столкнувшегося с угрозой "
-        "голода и истощения ресурсов Земли.",
-        time_film=2.50,
-    ),
-)
-storage.create(
-    MovieCreate(
-        slug="the-matrix-1999",
-        title_film="Матрица",
-        genre="Научная фантастика",
-        production_year=1999,
-        description_film="Хакер узнаёт, что мир вокруг — иллюзия, созданная искусственным разумом, "
-        "и присоединяется к борьбе за освобождение человечества от машинного контроля.",
-        time_film=2.45,
-    ),
-)
-storage.create(
-    MovieCreate(
-        slug="inception-2010",
-        title_film="Начало",
-        genre="Научная фантастика",
-        production_year=2010,
-        description_film="Группа специалистов проникает в подсознание людей, используя технику погружения в "
-        "сновидения.",
-        time_film=2.28,
-    ),
-)
+# storage.create(
+#     MovieCreate(
+#         slug="interstellar-2014",
+#         title_film="Интерстеллар",
+#         genre="Научная фантастика",
+#         production_year=2014,
+#         description_film="Фильм «Интерстеллар» рассказывает историю группы астронавтов, отправившихся сквозь "
+#         "червоточину в космос в поисках нового дома для человечества, столкнувшегося с угрозой "
+#         "голода и истощения ресурсов Земли.",
+#         time_film=2.50,
+#     ),
+# )
+# storage.create(
+#     MovieCreate(
+#         slug="the-matrix-1999",
+#         title_film="Матрица",
+#         genre="Научная фантастика",
+#         production_year=1999,
+#         description_film="Хакер узнаёт, что мир вокруг — иллюзия, созданная искусственным разумом, "
+#         "и присоединяется к борьбе за освобождение человечества от машинного контроля.",
+#         time_film=2.45,
+#     ),
+# )
+# storage.create(
+#     MovieCreate(
+#         slug="inception-2010",
+#         title_film="Начало",
+#         genre="Научная фантастика",
+#         production_year=2010,
+#         description_film="Группа специалистов проникает в подсознание людей, используя технику погружения в "
+#         "сновидения.",
+#         time_film=2.28,
+#     ),
+# )
