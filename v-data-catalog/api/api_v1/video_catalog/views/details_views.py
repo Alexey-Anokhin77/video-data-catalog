3056875d637d0574823a20f4bd213c1cb3b67173
Alexey Anokhin <alexey.anokhin88@gmail.com>
2025-05-20 10:40:57 +0300

    Add Background Tasks

--- v-data-catalog/api/api_v1/video_catalog/views/details_views.py
+++ v-data-catalog/api/api_v1/video_catalog/views/details_views.py
@@ -1,6 +1,10 @@
 from typing import Annotated
 
-from fastapi import APIRouter, Depends
+from fastapi import (
+    APIRouter,
+    Depends,
+    BackgroundTasks,
+)
 from starlette import status
 
 from api.api_v1.video_catalog.crud import storage
@@ -52,7 +56,9 @@ def read_film_details(
 def update_movie_details(
     film: MovieBySlug,
     film_in: MovieUpdate,
+    background_tasks: BackgroundTasks,
 ):
+    background_tasks.add_task(storage.save_state),
     return storage.update(
         film=film,
         film_in=film_in,
@@ -66,7 +72,9 @@ def update_movie_details(
 def update_movie_details_partial(
     film: MovieBySlug,
     film_in: MoviePartialUpdate,
+    background_tasks: BackgroundTasks,
 ) -> Movie:
+    background_tasks.add_task(storage.save_state),
     return storage.partial_update(
         film=film,
         film_in=film_in,
@@ -79,5 +87,7 @@ def update_movie_details_partial(
 )
 def delete_film(
     film: MovieBySlug,
+    background_tasks: BackgroundTasks,
 ) -> None:
     storage.delete(film=film)
+    background_tasks.add_task(storage.save_state)
