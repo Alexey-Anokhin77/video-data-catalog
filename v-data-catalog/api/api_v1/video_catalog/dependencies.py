67a5b2063687ccee75c302878f186b1820f99baf
Alexey Anokhin <alexey.anokhin88@gmail.com>
2025-05-21 14:25:45 +0300

    Create new def save storage state

--- v-data-catalog/api/api_v1/video_catalog/dependencies.py
+++ v-data-catalog/api/api_v1/video_catalog/dependencies.py
@@ -1,8 +1,13 @@
-from fastapi import HTTPException
+import logging
+
+from fastapi import HTTPException, BackgroundTasks
 from starlette import status
 
 from api.api_v1.video_catalog.crud import storage
 from schemas.video_catalog import Movie
+
+
+log = logging.getLogger(__name__)
 
 
 def read_film_slug(
@@ -15,3 +20,13 @@ def read_film_slug(
         status_code=status.HTTP_404_NOT_FOUND,
         detail=f"Film{slug!r} not found",
     )
+
+
+def save_storage_state(
+    background_tasks: BackgroundTasks,
+):
+    # сначала код до входа внутрь view функции
+    yield
+    # код после покидания view функции
+    log.info("Add background task to save_storage")
+    background_tasks.add_task(storage.save_state)
