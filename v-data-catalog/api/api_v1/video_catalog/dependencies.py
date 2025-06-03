9eb5ed376e0d9f730ff328f39dfa1a5370668dbc
Alexey Anokhin <alexey.anokhin88@gmail.com>
2025-06-03 15:46:14 +0300

    Create users basic auth

--- v-data-catalog/api/api_v1/video_catalog/dependencies.py
+++ v-data-catalog/api/api_v1/video_catalog/dependencies.py
@@ -5,19 +5,20 @@ from fastapi import (
     HTTPException,
     BackgroundTasks,
     Request,
-    Header,
 )
 from fastapi.params import Depends
 
 from fastapi.security import (
     HTTPAuthorizationCredentials,
     HTTPBearer,
+    HTTPBasicCredentials,
+    HTTPBasic,
 )
 
 from starlette import status
 
 from api.api_v1.video_catalog.crud import storage
-from core.config import API_TOKENS
+from core.config import API_TOKENS, USERS_DB
 from schemas.video_catalog import Movie
 
 
@@ -32,9 +33,16 @@ UNSAFE_METHOD = frozenset(
     }
 )
 
+
 static_api_token = HTTPBearer(
     scheme_name="Static API token",
     description="Your **Static API token** from developer portal. [Read more](#)",
+    auto_error=False,
+)
+
+user_basic_auth = HTTPBasic(
+    scheme_name="Basic Auth",
+    description="Basic username + password auth",
     auto_error=False,
 )
 
@@ -86,3 +94,29 @@ def api_token_required_for_unsafe_methods(
             status_code=status.HTTP_401_UNAUTHORIZED,
             detail="Invalid API token",
         )
+
+
+def basic_user_auth_required_for_unsafe_methods(
+    request: Request,
+    credentials: Annotated[
+        HTTPBasicCredentials | None,
+        Depends(user_basic_auth),
+    ] = None,
+):
+    log.info("Users auth credentials: %s", credentials)
+
+    if request.method not in UNSAFE_METHOD:
+        return
+
+    if (
+        credentials
+        and credentials.username in USERS_DB
+        and USERS_DB[credentials.username] == credentials.password
+    ):
+        return
+
+    raise HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="User credentials required. Incorrect username or password.",
+        headers={"WWW-Authenticate": "Basic"},
+    )
