12ec595761486d58e40b5dd2ce0aac284783879c
Alexey Anokhin <alexey.anokhin88@gmail.com>
2025-08-28 18:09:42 +0300

    Create new test for create, update and partial update

--- v-data-catalog/testing/test_api/test_api_v1/test_video_catalog/test_crud.py
+++ v-data-catalog/testing/test_api/test_api_v1/test_video_catalog/test_crud.py
@@ -1,6 +1,15 @@
 import random
+import string
 from os import getenv
 from unittest import TestCase
+
+from api.api_v1.video_catalog.crud import storage
+from schemas.video_catalog import (
+    Movie,
+    MovieCreate,
+    MoviePartialUpdate,
+    MovieUpdate,
+)
 
 if getenv("TESTING") != "1":
     msg = "Environment is not ready for testing"
@@ -11,10 +20,66 @@ def total(a: int, b: int) -> int:
     return a + b
 
 
-class TotalTestCase(TestCase):
-    def test_total(self) -> None:
-        num_a = random.randint(1, 100)
-        num_b = random.randint(1, 100)
-        result = total(num_a, num_b)
-        expected_result = num_a + num_b
-        self.assertEqual(expected_result, result)
+class VideoStorageUpdateTestCase(TestCase):
+    def setUp(self) -> None:
+        self.movie = self.create_movie()
+
+    def tearDown(self) -> None:
+        storage.delete(self.movie)
+
+    def create_movie(self) -> Movie:
+        movie_in = MovieCreate(
+            slug="".join(
+                random.choices(
+                    string.ascii_letters,
+                    k=8,
+                ),
+            ),
+            description_film="Some description",
+            time_film=1,
+            title_film="Some Title",
+            genre="Some Genre",
+            production_year=1901,
+        )
+        return storage.create(movie_in)
+
+    def test_update(self) -> None:
+        movie_update = MovieUpdate(
+            **self.movie.model_dump(),
+        )
+        source_description = self.movie.description_film
+        movie_update.description_film *= 2
+        updated_movie = storage.update(
+            movie=self.movie,
+            movie_in=movie_update,
+        )
+
+        self.assertNotEqual(
+            source_description,
+            updated_movie.description_film,
+        )
+
+        self.assertEqual(
+            movie_update,
+            MovieUpdate(**updated_movie.model_dump()),
+        )
+
+    def test_partial_update(self) -> None:
+        movie_partial_update = MoviePartialUpdate(
+            description_film=self.movie.description_film * 2,
+        )
+        source_description = self.movie.description_film
+        updated_movie = storage.partial_update(
+            movie=self.movie,
+            movie_in=movie_partial_update,
+        )
+
+        self.assertNotEqual(
+            source_description,
+            updated_movie.description_film,
+        )
+
+        self.assertEqual(
+            movie_partial_update.description_film,
+            updated_movie.description_film,
+        )
