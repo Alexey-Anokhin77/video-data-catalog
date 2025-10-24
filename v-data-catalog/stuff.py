0bb96e774c53507ac67200d5bdb41ac95ccc702d
Alexey Anokhin <alexey.anokhin88@gmail.com>
2025-10-24 14:39:38 +0300

    Update stuff

--- v-data-catalog/stuff.py
+++ v-data-catalog/stuff.py
@@ -8,3 +8,30 @@ redis = Redis(
     db=config.REDIS_DB,
     decode_responses=True,
 )
+
+
+def add(a: int, b: int) -> int:
+    return a + b
+
+
+def main() -> None:
+    print(redis.ping())
+    print(redis.ping())
+
+    redis.set("name", "Alexey")
+    redis.set("foo", "bar")
+    redis.set("number", "42")
+    print("name", redis.get("name"))
+    print(
+        [
+            redis.get("foo"),
+            redis.get("number"),
+            redis.get("spam"),
+        ],
+    )
+    redis.delete("name")
+    print("name", redis.get("name"))
+
+
+if __name__ == "__main__":
+    main()
