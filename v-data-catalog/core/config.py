2de4468822cb1c9b62d49d2ca6fab0191cc49c02
Alexey <alexey.anokhin88@gmail.com>
2025-11-12 15:52:20 +0300

    Add pydanitc-settings

--- v-data-catalog/core/config.py
+++ v-data-catalog/core/config.py
@@ -1,19 +1,15 @@
 import logging
-from os import getenv
 from pathlib import Path
+from pydantic_settings import BaseSettings
+from pydantic import BaseModel
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 MOVIES_STORAGE_FILEPATH = BASE_DIR / "movies.json"
 
-
-LOG_LEVEL = logging.INFO
 LOG_FORMAT: str = (
     "[%(asctime)s.%(msecs)03d] %(module)10s:%(lineno)-3d %(levelname)-7s - %(message)s"
 )
 
-
-REDIS_HOST = getenv("REDIS_HOST", "localhost")
-REDIS_PORT = int(getenv("REDIS_PORT", "0")) or 6379
 REDIS_DB = 0
 REDIS_DB_TOKENS = 1
 REDIS_DB_USERS = 2
@@ -22,3 +18,26 @@ REDIS_DB_MOVIES = 3
 
 REDIS_TOKENS_SET_NAME = "tokens"
 REDIS_MOVIES_HASH_NAME = "movies"
+
+
+class LoggingConfig(BaseModel):
+    log_level: int = logging.INFO
+    log_format: str = LOG_FORMAT
+    date_format: str = "%Y-%m-%d %H:%M:%S"
+
+
+class RedisConnectionConfig(BaseModel):
+    host: str = "localhost"
+    port: int = 6379
+
+
+class RedisConfig(BaseModel):
+    connection: RedisConnectionConfig = RedisConnectionConfig()
+
+
+class Settings(BaseSettings):
+    logging: LoggingConfig = LoggingConfig()
+    redis: RedisConfig = RedisConfig()
+
+
+settings = Settings()
